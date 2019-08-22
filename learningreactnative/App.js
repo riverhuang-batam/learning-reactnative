b58321d320171b49b067a083d5c35de443ee6b12
riverhuang-batam <riverhuang011@gmail.com>
2019-08-22 08:57:25 +0700

    installation expo

---
+++ learningreactnative/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  render(){
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button onPress={this._onPressButton}
+            title="Press me"
+            />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button onPress={this._onPressButton}
+            title="Press me"
+            color="#841584"
+            />
+      </View>
+      <View style={styles.alternativeLayoutButtonContainer}>
+        <Button onPress={this._onPressButton}
+            title="this Look great!"/>
+        <Button onPress={this._onPressButton}
+            title="OK!"
+            color="#841584"
+            />
+      </View>
+    </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonContainer:{
+    margin: 20
+  },
+  alternativeLayoutButtonContainer:{
+    margin:20,
+    flexDirection:'row',
+    justifyContent:'space-between'
+  }
+});
